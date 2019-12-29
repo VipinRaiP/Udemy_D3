54610710fcb038e19de54b5aa9c28c49113b69df
VipinRaiP <vipin.rai.p@gmail.com>
2019-12-29 11:41:56 +0530

    first commit

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -3,7 +3,7 @@
 *    Mastering Data Visualization with D3.js
 *    2.4 - Adding SVGs with D3
 */
-
+/*
 var svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
 	.attr("height", 400);
@@ -12,4 +12,15 @@ var circle = svg.append("circle")
 	.attr("cx", 100)
 	.attr("cy", 250)
 	.attr("r", 70)
-	.attr("fill", "grey");
+	.attr("fill", "grey"); */
+
+
+	var svg = d3.select("#chart-area").append("svg")
+						.attr("width",400)
+						.attr("height",400)
+
+ var circle = svg.append("circle")
+ 							.attr("cx",200)
+							.attr("cy",200)
+							.attr("r",70)
+							.attr("fill","red")
